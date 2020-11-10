de8f4f43fb9638708d686ab49f9d525d24ed0447
DmanCoder <ktdenis.1@live.com>
2020-11-10 10:43:02 +1000

    Initial

--- src/gsapConfig.js
+++
@@ -1,7 +0,0 @@
-import { gsap } from 'gsap';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
-
-export default gsap;
