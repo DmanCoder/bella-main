82b876c8f0a152d2f99cbc48fd715e93e620b825
DmanCoder <ktdenis.1@live.com>
2020-11-10 16:19:15 +1000

    Destructured events

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ gsap.registerPlugin(ScrollTrigger);
 
 function initNavigation() {
   const mainNavLinks = gsap.utils.toArray('.main-nav a');
+  const mainNavLinksRev = gsap.utils.toArray('.main-nav a').reverse();
+
   mainNavLinks.forEach((link) => {
     link.addEventListener('mouseleave', (e) => {
       // Add class
@@ -13,10 +15,45 @@ function initNavigation() {
       }, 300);
     });
   });
+
+  function navAnimation(direction) {
+    const scrollingDown = direction === 1;
+    const links = scrollingDown ? mainNavLinks : mainNavLinksRev;
+
+    return gsap.to(links, {
+      duration: 0.3,
+      stagger: 0.05,
+      autoAlpha: () => (scrollingDown ? 0 : 1),
+      y: () => (scrollingDown ? 20 : 0),
+      ease: 'power4.out',
+    });
+  }
+
+  ScrollTrigger.create({
+    start: 100,
+    end: 'bottom bottom-=200',
+    toggleClass: {
+      targets: 'body',
+      className: 'has-scrolled',
+    },
+    onEnter: ({ direction }) => navAnimation(direction),
+    onLeaveBack: ({ direction }) => navAnimation(direction),
+  });
+}
+
+function moveImages(e) {
+  const { offsetX, offsetY, target } = e;
+  const { clientWidth, clientHeight } = target;
+  console.log(offsetX, offsetY, clientHeight, clientWidth);
+}
+
+function initHeaderTilt() {
+  document.querySelector('header').addEventListener('mousemove', moveImages);
 }
 
 function init() {
   initNavigation();
+  initHeaderTilt();
 }
 
 window.addEventListener('load', function () {
